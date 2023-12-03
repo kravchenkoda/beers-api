69781404faac0695aacc533c8f3a97aef548bbb0
kravchenkoda <dmytro.kravchenko.a@gmail.com>
2023-12-03 22:51:36 +0200

    Added metadata with beer schema to db.py

--- app/db.py
+++ app/db.py
@@ -1,14 +1,18 @@
 import os
 from urllib.parse import quote_plus
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.engine import URL
+from sqlalchemy.orm import declarative_base
 
 PG_USER = os.environ['PGUSER']
 PG_PASSWORD = quote_plus(os.environ['PGPASSWORD'])
 PG_DATABASE = os.environ['PGDATABASE']
 PG_HOST = os.environ['PGHOST']
 PG_PORT = int(os.environ['PGPORT'])
+
+metadata = MetaData(schema='beer')
+Base = declarative_base(metadata=metadata)
 
 url = URL.create(
     drivername='postgresql',
